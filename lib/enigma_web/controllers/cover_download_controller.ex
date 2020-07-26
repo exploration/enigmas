0efbf2428ef720136b0b77002511af7fbfe1eb37
Donald Merand <dmerand@explo.org>
2020-07-26 15:03:07 -0400

    remove inspect

--- lib/enigma_web/controllers/cover_download_controller.ex
+++ lib/enigma_web/controllers/cover_download_controller.ex
@@ -4,7 +4,6 @@ defmodule EnigmaWeb.CoverDownloadController do
 
   def show(conn, %{"id" => blob} = params) do
     {:ok, cover} = Cover.decode64 blob
-    IO.inspect params
     svg = case params do
       %{"variety" => "rectangle"} ->
         Renderer.render_cover_rectangle(cover) 
